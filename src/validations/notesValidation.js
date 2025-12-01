e5d20285a16fa5210b06433fb13d29fe572290db
engapantio <bohdankozar@zohomail.eu>
2025-12-01 17:45:54 +0100

    fix validation messaging

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -9,16 +9,38 @@ const objectIdValidator = (value, helpers) =>
 
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
-    tag: Joi.string().valid(...TAGS),
-    search: Joi.string().trim().allow(''),
-    page: Joi.number().integer().min(1).default(1),
-    perPage: Joi.number().integer().min(2).max(25).default(10),
+    tag: Joi.string()
+      .pattern(/[A-Z][a-z]+/)
+      .valid(...TAGS),
+    search: Joi.string().trim().allow('').messages({
+      'string.base': 'Search must be a string',
+      'string.trim': 'Search should not contain whitespace around it.',
+    }),
+    page: Joi.number().integer().min(1).default(1).messages({
+      'number.base': 'Page must be a number',
+      'number.integer': 'Page must be an integer number',
+      'number.min': 'Page should not be lower than {#limit}',
+    }),
+    perPage: Joi.number().integer().min(2).max(25).default(10).messages({
+      'number.base': 'PerPage must be a number',
+      'number.integer': 'PerPage must be an integer number',
+      'number.min': 'PerPage should be at least {#limit}',
+      'number.max': 'PerPage should not be more than {#limit}',
+    }),
   }),
 };
 
 export const noteIdSchema = {
   [Segments.PARAMS]: Joi.object({
-    noteId: Joi.string().custom(objectIdValidator).required(),
+    noteId: Joi.string()
+      .alphanum()
+      .custom(objectIdValidator)
+      .required()
+      .messages({
+        'string.base': 'NoteId must be a string',
+        'string.alphanum': 'NoteId must contain alphanumeric characters only',
+        'any.required': 'NoteId is required',
+      }),
   }),
 };
 
@@ -31,11 +53,13 @@ export const createNoteSchema = {
     }),
     content: Joi.string().allow('').messages({
       'string.base': 'Content must be a string',
-      'string.allow': 'Content can be an empty string',
     }),
     tag: Joi.string()
+      .pattern(/[A-Z][a-z]+/)
       .valid(...TAGS)
       .messages({
+        'string.base': 'Tag must be a string',
+        'string.pattern.base': 'Tag must start with capital letter',
         'any.only': `Tag must be one of: {...TAGS}`,
       }),
   }),
@@ -43,22 +67,25 @@ export const createNoteSchema = {
 
 export const updateNoteSchema = {
   ...noteIdSchema,
-  // [Segments.PARAMS]: Joi.object({
-  //   noteId: Joi.string().custom(objectIdValidator).required(),
-  // }),
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).messages({
       'string.base': 'Title must be a string',
       'string.min': 'Title should have at least {#limit} characters',
     }),
-    content: Joi.string().min(0).messages({
+    content: Joi.string().allow('').messages({
       'string.base': 'Content must be a string',
-      'string.min': 'Content must be at least {#limit}',
     }),
     tag: Joi.string()
+      .pattern(/[A-Z][a-z]+/)
       .valid(...TAGS)
       .messages({
-        'any.only': `Tag must be one of: {TAGS}`,
+        'string.base': 'Tag must be a string',
+        'string.pattern.base': 'Tag must start with capital letter',
+        'any.only': `Tag must be one of: {...TAGS}`,
       }),
-  }).min(1),
+  })
+    .min(1)
+    .messages({
+      'object.min': 'At least {#limit} field should be provided for update',
+    }),
 };
